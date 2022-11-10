013e9d62ae4555029633d028e2ad217a17dcfe65
bruvio <bruno.viola@pm.me>
2022-11-10 14:44:44 +0000

    fix key

--- bastion.tf
+++ bastion.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "bastion" {
 
   user_data            = file("${path.module}/templates/bastion/user-data.sh")
   iam_instance_profile = aws_iam_instance_profile.bastion.name
-  key_name             = var.bastion_key_name
+  key_name             = "${var.project}-${var.bastion_key_name}"
   subnet_id            = aws_subnet.public_a.id
   vpc_security_group_ids = [
     aws_security_group.bastion.id
--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,11 @@ output "api_endpoint" {
   value = aws_route53_record.app.fqdn
 }
 output "state-bucket" {
-value = aws_s3_bucket.terraform_state.id 
+  value = aws_s3_bucket.terraform_state.id
 }
 
 output "dynamo_db_table" {
-value = aws_dynamodb_table.terraform_lock.name
+  value = aws_dynamodb_table.terraform_lock.name
 }
 
 
