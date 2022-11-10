256794679ec98b5d27724d7c67e9ebad2c3c3094
bruvio <bruno.viola@pm.me>
2022-11-10 14:14:39 +0000

    fix state bucket name

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
 
-  bucket = var.bucket_name
+  bucket = var.state_bucket
 
   // This is only here so we can destroy the bucket in development. This is not for production
   // usage
