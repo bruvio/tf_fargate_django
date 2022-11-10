6a5e5d304e160d3c525348331181d0bef4230d3c
bruvio <bruno.viola@pm.me>
2022-11-10 20:23:52 +0000

    marking output as sensitive

--- outputs.tf
+++ outputs.tf
@@ -18,12 +18,14 @@ output "access-key-proxy-id" {
   value = aws_iam_access_key.access-key-proxy.id
 }
 output "access-key-proxy-secret" {
+  sensitive = true
   value = aws_iam_access_key.access-key-proxy.secret
 }
 output "access-key-id" {
   value = aws_iam_access_key.access-key.id
 }
 output "access-key-secret" {
+  sensitive = true
   value = aws_iam_access_key.access-key.secret
 }
 
