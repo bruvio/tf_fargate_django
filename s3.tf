2779f5c22ef93c80e9f28e95e4e32b4dba4cad73
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-09 23:17:11 +0000

    fix: improve s3

--- s3.tf
+++ s3.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "app_public_files" {
   bucket        = "${var.project}-${var.bucket_name}"
   force_destroy = var.force_destroy
+  # acl           = "public-read"
 
   tags = var.common_tags
 }
 
-
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket                  = aws_s3_bucket.bucket.id
+  bucket                  = aws_s3_bucket.app_public_files.id
   block_public_acls       = var.allow_public_access ? false : true
   block_public_policy     = var.allow_public_access ? false : true
   ignore_public_acls      = var.allow_public_access ? false : true
@@ -21,155 +21,3 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
     status = var.disable_versioning ? "Disabled" : "Enabled"
   }
 }
-/////////////////////////////////
-//      Bucket LIFECYCLEs      //
-////////////////////////////////
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  count = length(local.lifecycle_rules) > 0 ? 1 : 0
-
-  bucket                = aws_s3_bucket.bucket.id
-  expected_bucket_owner = var.expected_bucket_owner
-
-  dynamic "rule" {
-    for_each = local.lifecycle_rules
-
-    content {
-      id     = try(rule.value.id, null)
-      status = try(rule.value.enabled ? "Enabled" : "Disabled", tobool(rule.value.status) ? "Enabled" : "Disabled", title(lower(rule.value.status)))
-
-      # Max 1 block - abort_incomplete_multipart_upload
-      dynamic "abort_incomplete_multipart_upload" {
-        for_each = try([rule.value.abort_incomplete_multipart_upload_days], [])
-
-        content {
-          days_after_initiation = try(rule.value.abort_incomplete_multipart_upload_days, null)
-        }
-      }
-
-
-      # Max 1 block - expiration
-      dynamic "expiration" {
-        for_each = try(flatten([rule.value.expiration]), [])
-
-        content {
-          date                         = try(expiration.value.date, null)
-          days                         = try(expiration.value.days, null)
-          expired_object_delete_marker = try(expiration.value.expired_object_delete_marker, null)
-        }
-      }
-
-      # Several blocks - transition
-      dynamic "transition" {
-        for_each = try(flatten([rule.value.transition]), [])
-
-        content {
-          date          = try(transition.value.date, null)
-          days          = try(transition.value.days, null)
-          storage_class = transition.value.storage_class
-        }
-      }
-
-      # Max 1 block - noncurrent_version_expiration
-      dynamic "noncurrent_version_expiration" {
-        for_each = try(flatten([rule.value.noncurrent_version_expiration]), [])
-
-        content {
-          newer_noncurrent_versions = try(noncurrent_version_expiration.value.newer_noncurrent_versions, null)
-          noncurrent_days           = try(noncurrent_version_expiration.value.days, noncurrent_version_expiration.value.noncurrent_days, null)
-        }
-      }
-
-      # Several blocks - noncurrent_version_transition
-      dynamic "noncurrent_version_transition" {
-        for_each = try(flatten([rule.value.noncurrent_version_transition]), [])
-
-        content {
-          newer_noncurrent_versions = try(noncurrent_version_transition.value.newer_noncurrent_versions, null)
-          noncurrent_days           = try(noncurrent_version_transition.value.days, noncurrent_version_transition.value.noncurrent_days, null)
-          storage_class             = noncurrent_version_transition.value.storage_class
-        }
-      }
-
-      # Max 1 block - filter - without any key arguments or tags
-      dynamic "filter" {
-        for_each = length(try(flatten([rule.value.filter]), [])) == 0 ? [true] : []
-
-        content {
-          #          prefix = ""
-        }
-      }
-
-      # Max 1 block - filter - with one key argument or a single tag
-      dynamic "filter" {
-        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) == 1]
-
-        content {
-          object_size_greater_than = try(filter.value.object_size_greater_than, null)
-          object_size_less_than    = try(filter.value.object_size_less_than, null)
-          prefix                   = try(filter.value.prefix, null)
-
-          dynamic "tag" {
-            for_each = try(filter.value.tags, filter.value.tag, [])
-
-            content {
-              key   = tag.key
-              value = tag.value
-            }
-          }
-        }
-      }
-
-      # Max 1 block - filter - with more than one key arguments or multiple tags
-      dynamic "filter" {
-        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) > 1]
-
-        content {
-          and {
-            object_size_greater_than = try(filter.value.object_size_greater_than, null)
-            object_size_less_than    = try(filter.value.object_size_less_than, null)
-            prefix                   = try(filter.value.prefix, null)
-            tags                     = try(filter.value.tags, filter.value.tag, null)
-          }
-        }
-      }
-    }
-  }
-
-  # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.bucket_versioning]
-}
-
-
-/////////////////////////
-///    Bucket LOGs    ///
-////////////////////////
-resource "aws_s3_bucket_logging" "this" {
-  count = length(keys(var.logging)) > 0 ? 1 : 0
-
-  bucket = aws_s3_bucket.bucket.id
-
-  target_bucket = var.logging["target_bucket"]
-  target_prefix = try(var.logging["target_prefix"], null)
-
-
-  dynamic "target_object_key_format" {
-    for_each = try([var.logging["target_object_key_format"]], [])
-
-    content {
-      dynamic "partitioned_prefix" {
-        for_each = try(target_object_key_format.value["partitioned_prefix"], [])
-
-        content {
-          partition_date_source = try(partitioned_prefix.value, null)
-        }
-      }
-
-      dynamic "simple_prefix" {
-        for_each = contains(keys(target_object_key_format.value), "simple_prefix") ? [true] : []
-
-        content {}
-      }
-    }
-  }
-}
-
