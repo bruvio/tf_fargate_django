d78e2496b71468b0720613a6e0170c6c7bdaae5a
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-09 22:12:34 +0000

    fix: enforce immutable tags

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
 #tfsec:ignore:aws-ecr-enforce-immutable-repository
 resource "aws_ecr_repository" "app" {
   name                 = var.project
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   tags                 = var.common_tags
   # count                = var.vpc_name == "dev" ? 1 : 0
 
@@ -18,7 +18,7 @@ resource "aws_ecr_repository" "app" {
 
 resource "aws_ecr_repository" "proxy" {
   name                 = "${var.project}-proxy"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   tags                 = var.common_tags
   # count                = var.vpc_name == "dev" ? 1 : 0
 
