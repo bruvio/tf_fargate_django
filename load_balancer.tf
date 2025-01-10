d975b809c80c9b5466f2c51cbb94c431bb842c88
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-10 04:50:28 +0000

    fix: add custom host in health check

--- load_balancer.tf
+++ load_balancer.tf
@@ -65,7 +65,7 @@ resource "aws_lb_listener_rule" "host_header_rule" {
     }
   }
 
-  actions {
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.api.arn
   }
