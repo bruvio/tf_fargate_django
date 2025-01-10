852b0d87ad2b09469932c5bacc5f42e33436aaae
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-10 00:14:15 +0000

    fix: health checks

--- ecs.tf
+++ ecs.tf
@@ -258,6 +258,15 @@ resource "aws_security_group" "ecs_service" {
   tags = var.common_tags
 }
 
+resource "aws_security_group_rule" "allow_alb_health_checks" {
+  type                     = "ingress"
+  from_port                = 8000
+  to_port                  = 8000
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_service.id
+  source_security_group_id = aws_security_group.lb.id
+}
+
 # ----------------------------
 # ECS Service
 # ----------------------------
