e44cd90d83180de3caab3ed2517ee4d94ef48417
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-10 10:09:58 +0000

    fix: add lifecycle to alb health check rule

--- ecs.tf
+++ ecs.tf
@@ -249,20 +249,13 @@ resource "aws_security_group" "ecs_service" {
     cidr_blocks = module.vpc.private_subnets_cidr_blocks
   }
 
-  ingress {
-    from_port       = 9000
-    to_port         = 9000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb.id]
-  }
-
   tags = var.common_tags
 }
 
 resource "aws_security_group_rule" "allow_alb_health_checks" {
   type                     = "ingress"
   from_port                = 8000
-  to_port                  = 8000
+  to_port                  = 9000
   protocol                 = "tcp"
   security_group_id        = aws_security_group.ecs_service.id
   source_security_group_id = aws_security_group.lb.id
