539b94a65068937b9cd3efa0bb14ef75166c8bf7
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-11 06:28:32 +0000

    feat: ssh into container

--- variables.tf
+++ variables.tf
@@ -144,7 +144,7 @@ variable "disable_versioning" {
   type        = bool
   default     = false
 }
-variable "enable_execute_command " {
+variable "enable_execute_command" {
   description = "whether to enable ssh into container"
   default     = true
 }
