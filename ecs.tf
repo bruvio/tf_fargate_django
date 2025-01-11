0137b66c3f3d4d5dfd9a1d4dbe2bf3e48f18da06
bruvio <bruno.viola@genomicsengland.co.uk>
2025-01-11 07:42:34 +0000

    fix: remove prevent destroy

--- ecs.tf
+++ ecs.tf
@@ -298,9 +298,9 @@ resource "aws_security_group_rule" "allow_alb_health_checks" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.ecs_service.id
   source_security_group_id = aws_security_group.lb.id
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 # ----------------------------
